dcb1103f625ad3e450cf8b1774cb6889e2100893
takeuchikatsuchi <k64katu195@gmail.com>
2020-05-06 15:33:26 +0900

    6

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-'use stript'
+'use strict';
+
+{
+    class Panel {
+        constructor() {
+            const section = document.createElement('section');
+            section.classList.add('panel');
+
+            this.img = document.createElement('img')
+            this.img.src = 'img/seven.png';
+
+            this.stop = document.createElement('div');
+            this.stop.textContent = 'STOP';
+            this.stop.classList.add('stop');
+        }
+    }
+}
